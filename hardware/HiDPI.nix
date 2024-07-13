60465e227eb92cd80fe393ad21182c5bb7030b48
kjartanpog <kjartan.masson@proton.me>
2024-07-13 11:11:23 +0000

    Gnome HiDPI for Z13

--- hardware/HiDPI.nix
+++ hardware/HiDPI.nix
@@ -1,7 +1,9 @@
 { config, lib, ... }: {
 
+  environment.sessionVariables.HIDPI = "1";
+
   services.xserver.desktopManager.gnome.extraGSettingsOverrides =
-    lib.mkIf config.services.xserver.desktopManager.gnome.enable ''
+    lib.mkIf ( config.services.xserver.desktopManager.gnome.enable == true ) ''
       [org.gnome.mutter]
       experimental-features=[ 'scale-monitor-framebuffer' ]
     '';
