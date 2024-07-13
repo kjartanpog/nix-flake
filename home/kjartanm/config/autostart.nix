b19d31e3cee9f6db13547ddb7b0d9e5ef147d97d
kjartanpog <kjartan.masson@proton.me>
2024-07-13 21:02:27 +0000

    proton-mail-bridge autostart

--- home/kjartanm/config/autostart.nix
+++ home/kjartanm/config/autostart.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }: {
-  home.file.".config/autostart/com.proton-bridge-gui.desktop".source = "${pkgs.protonmail-bridge}/share/applications/proton-bridge-gui.desktop";
+  home.file.".config/autostart/com.proton-bridge-gui.desktop".source = "${pkgs.protonmail-bridge-gui}/share/applications/proton-bridge-gui.desktop";
   home.file.".config/autostart/com.hunterwittenborn.Celeste.desktop".source = "${pkgs.celeste}/share/applications/com.hunterwittenborn.Celeste.desktop";
 }
