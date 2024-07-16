c91dba8a4a7dd7d0f2d3b3319ecfd86761bc631d
kjartanpog <kjartan.masson@proton.me>
2024-07-16 20:32:43 +0000

    Inter font conf features

--- home/kjartanm/fonts.nix
+++ home/kjartanm/fonts.nix
@@ -1,6 +1,22 @@
 { pkgs, ... }: {
-  fonts.packages = with pkgs; [
-    inter
-    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
-  ];
+  fonts = {
+    packages = with pkgs; [
+      inter
+      (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+    ];
+    fontconfig = {
+      localConf = ''
+        <match target="font">
+            <test qual="any" name="family"><string>Inter</string></test>
+            <!-- These are set with "prepend" so that applications which configure features don't get overridden -->
+            <edit name="fontfeatures" mode="prepend">
+                <string>tnum</string><!-- Tabular numbers -->
+                <string>frac</string><!-- Fractions -->
+                <string>zero</string><!-- Slashed Zero -->
+                <string>ss02</string><!-- Disambiguation -->
+            </edit>
+        </match>
+      '';
+    };
+  };
 }
