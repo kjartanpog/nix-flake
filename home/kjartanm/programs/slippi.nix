061d73254ca341ca92f1a68c197fd8a23ae52814
kjartanpog <kjartan.masson@proton.me>
2024-07-29 11:09:27 +0000

    fix slippi ISO path

--- home/kjartanm/programs/slippi.nix
+++ home/kjartanm/programs/slippi.nix
@@ -9,7 +9,7 @@
   slippi.launcher = {
     enable = true;
     # isoPath = "Path/To/SSBM.ciso";
-    isoPath = "${config.home.homeDirectory}/Games/Slippi/Super Smash Bros. Melee (USA) (v1.02).iso";
+    isoPath = "${config.home.homeDirectory}/Games/ROMs/GC/Slippi/Super Smash Bros. Melee (USA) (v1.02).iso";
     netplayVersion = "3.4.1";
     netplayHash = "sha256-uqy9YQnvryTHmskzlX+4st1VacnHbpC2gdC+PgvFUlA=";
     # playbackVersion = "3.4.0";
