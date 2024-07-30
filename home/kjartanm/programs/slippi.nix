fae572d50f185aaff68485401f72db9659de40d1
kjartanpog <kjartan.masson@proton.me>
2024-07-30 22:17:41 +0000

    Slippi 3.4.2

--- home/kjartanm/programs/slippi.nix
+++ home/kjartanm/programs/slippi.nix
@@ -11,8 +11,8 @@
     # isoPath = "Path/To/SSBM.ciso";
     # isoPath = "${config.home.homeDirectory}/Games/ROMs/GC/Slippi/Super Smash Bros. Melee (USA) (v1.02).iso";
     isoPath = "${config.home.homeDirectory}/Games/ROMs/GC/melee.iso";
-    # netplayVersion = "3.4.1";
-    # netplayHash = "sha256-uqy9YQnvryTHmskzlX+4st1VacnHbpC2gdC+PgvFUlA=";
+    netplayVersion = "3.4.2";
+    netplayHash = "sha256-d1iawMsMwFElUqFmwWAD9rNsDdQr2LKscU8xuJPvxYg=";
     # playbackVersion = "3.4.0";
     # playbackHash = "sha256-d1iawMsMwFElUqFmwWAD9rNsDdQr2LKscU8xuJPvxYg=";
     launchMeleeOnPlay = false;
