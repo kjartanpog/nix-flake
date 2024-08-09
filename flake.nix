04a60825181a4ea5e9a0f08c9e644dde4bae1910
kjartanpog <kjartan.masson@proton.me>
2024-08-09 11:34:16 +0000

    lytedev/slippi-nix

--- flake.nix
+++ flake.nix
@@ -62,8 +62,8 @@
     };
 
     slippi-nix = {
-      # url = "github:lytedev/slippi-nix";
-      url = "github:kjartanpog/slippi-nix";
+      url = "github:lytedev/slippi-nix";
+      # url = "github:kjartanpog/slippi-nix";
     };
 
     nixvim = {
