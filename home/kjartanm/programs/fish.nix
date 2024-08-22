c14f5cc4c30dc617e2216ffce37e6bb6b9f01c11
kjartanpog <kjartan.masson@proton.me>
2024-08-22 16:40:30 +0000

    Fish shell vi mode

--- home/kjartanm/programs/fish.nix
+++ home/kjartanm/programs/fish.nix
@@ -1,5 +1,13 @@
 { ... }: {
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      fish_vi_key_bindings
+      function fish_mode_prompt; end
+      set fish_cursor_default block
+      set fish_cursor_insert line
+      set fish_cursor_replace_one underscore
+      set fish_cursor_replace underscore
+    '';
   };
 }
