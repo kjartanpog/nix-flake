dce149a9e19ba8eb269397bc25b3989c0d8aeb8d
kjartanpog <kjartan.masson@proton.me>
2024-08-22 18:36:57 +0000

    Fix Fish shell vi mode cursor for GVIM

--- home/kjartanm/programs/fish.nix
+++ home/kjartanm/programs/fish.nix
@@ -3,6 +3,7 @@
     enable = true;
     interactiveShellInit = ''
       fish_vi_key_bindings
+      set fish_vi_force_cursor 1
       function fish_mode_prompt; end
       set fish_cursor_default block
       set fish_cursor_insert line
